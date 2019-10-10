e9e134fa125d15b0f91891a5769b94af73f487f1
Daivid S. Silverio <daivid.silverio@gmail.com>
2019-10-10 13:34:14 -0400

    :construction: Bind menu visibility, actions and texts

--- Toggl.Droid/Fragments/Calendar/CalendarDayViewPageFragment.cs
+++ Toggl.Droid/Fragments/Calendar/CalendarDayViewPageFragment.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Android.OS;
 using Android.Support.V4.App;
+using Android.Text;
 using Android.Views;
 using Toggl.Core.UI.ViewModels.Calendar;
+using Toggl.Core.UI.ViewModels.Calendar.ContextualMenu;
 using Toggl.Core.UI.Views;
-using Toggl.Droid.Views.Calendar;
+using Toggl.Droid.Adapters;
+using Toggl.Droid.Extensions.Reactive;
+using Toggl.Droid.ViewHolders;
 using Toggl.Shared.Extensions;
 using Toggl.Shared.Extensions.Reactive;
+using TimeEntryExtensions = Toggl.Droid.Extensions.TimeEntryExtensions;
 
 namespace Toggl.Droid.Fragments.Calendar
 {
@@ -16,27 +22,31 @@ namespace Toggl.Droid.Fragments.Calendar
     {
         private readonly TimeSpan defaultTimeEntryDurationForCreation = TimeSpan.FromMinutes(30);
         private CompositeDisposable DisposeBag = new CompositeDisposable();
-        private CalendarDayView calendarDayView;
+        private IDisposable dismissActionDisposeBag;
+        private SimpleAdapter<CalendarMenuAction> menuActionsAdapter;
 
         public CalendarDayViewModel ViewModel { get; set; }
         public BehaviorRelay<int> CurrentPageRelay { get; set; }
         public BehaviorRelay<int> ScrollOffsetRelay { get; set; }
+        public BehaviorRelay<bool> MenuVisibilityRelay { get; set; }
         public IObservable<bool> ScrollToStartSign { get; set; }
             
         public int PageNumber { get; set; }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            calendarDayView = new CalendarDayView(Context);
-            return calendarDayView;
+            var view = inflater.Inflate(Resource.Layout.CalendarDayFragmentPage, container, false);
+            initializeViews(view);
+            return view;
         }
 
         public override void OnViewCreated(View view, Bundle savedInstanceState)
         {
             base.OnViewCreated(view, savedInstanceState);
             if (ViewModel == null) return;
-            
-            ViewModel?.AttachView(this);
+
+            ViewModel.AttachView(this);
+            ViewModel.ContextualMenuViewModel.AttachView(this);
             calendarDayView.SetCurrentDate(ViewModel.Date);
             calendarDayView.SetOffset(ScrollOffsetRelay?.Value ?? 0);
             calendarDayView.UpdateItems(ViewModel.CalendarItems);
@@ -46,7 +56,7 @@ namespace Toggl.Droid.Fragments.Calendar
                 .DisposedBy(DisposeBag);
 
             calendarDayView.CalendarItemTappedObservable
-                .Subscribe(ViewModel.OnItemTapped.Inputs)
+                .Subscribe(item => ViewModel.ContextualMenuViewModel.OnCalendarItemUpdated.Inputs.OnNext(item))
                 .DisposedBy(DisposeBag);
 
             calendarDayView.EmptySpansTouchedObservable
@@ -65,6 +75,77 @@ namespace Toggl.Droid.Fragments.Calendar
             calendarDayView.ScrollOffsetObservable
                 .Subscribe(updateScrollOffsetIfCurrentPage)
                 .DisposedBy(DisposeBag);
+
+            menuActionsAdapter = new SimpleAdapter<CalendarMenuAction>(
+                Resource.Layout.ContextualMenuActionCell,
+                CalendarMenuActionCellViewHolder.Create);
+            
+            actionsRecyclerView.SetAdapter(menuActionsAdapter);
+
+            ViewModel.ContextualMenuViewModel
+                .CurrentMenu
+                .Subscribe(updateMenuBindings)
+                .DisposedBy(DisposeBag);
+            
+            ViewModel.ContextualMenuViewModel
+                .MenuVisible
+                .Subscribe(contextualMenuContainer.Rx().IsVisible())
+                .DisposedBy(DisposeBag);
+            
+            ViewModel.ContextualMenuViewModel
+                .MenuVisible
+                .Subscribe(notifyMenuVisibilityIfCurrentPage)
+                .DisposedBy(DisposeBag);
+            
+            ViewModel.ContextualMenuViewModel
+                .TimeEntryPeriod
+                .Subscribe(periodText.Rx().TextObserver())
+                .DisposedBy(DisposeBag);
+            
+            ViewModel.ContextualMenuViewModel
+                .TimeEntryInfo
+                .Select(convertTimeEntryInfoToSpannable)
+                .Subscribe(timeEntryDetails.Rx().TextFormattedObserver())
+                .DisposedBy(DisposeBag);
+        }
+        private void updateMenuBindings(CalendarContextualMenu contextualMenu)
+        {
+            menuActionsAdapter.Items = contextualMenu.Actions;
+            dismissActionDisposeBag?.Dispose();
+            dismissActionDisposeBag = dismissButton.Rx().Tap()
+                .Subscribe(contextualMenu.Dismiss.Inputs);
+        }
+
+        private void notifyMenuVisibilityIfCurrentPage(bool menuIsVisible)
+        {
+            if (CurrentPageRelay?.Value == PageNumber)
+            {
+                MenuVisibilityRelay?.Accept(menuIsVisible);
+            }
+        }
+
+        private ISpannable convertTimeEntryInfoToSpannable(TimeEntryDisplayInfo timeEntryDisplayInfo)
+        {
+            var description = string.IsNullOrEmpty(timeEntryDisplayInfo.Description)
+                ? Shared.Resources.NoDescription
+                : timeEntryDisplayInfo.Description;
+            var hasProject = !string.IsNullOrEmpty(timeEntryDisplayInfo.Project);
+            var builder = new SpannableStringBuilder();
+            var projectTaskClient = TimeEntryExtensions.ToProjectTaskClient(
+                Context,
+                hasProject,
+                timeEntryDisplayInfo.Project,
+                timeEntryDisplayInfo.ProjectTaskColor,
+                timeEntryDisplayInfo.Task,
+                timeEntryDisplayInfo.Client,
+                false,
+                false
+            );
+            
+            builder.Append(description);
+            builder.Append(" ");
+            builder.Append(projectTaskClient);
+            return builder;
         }
 
         private void updateScrollOffsetIfCurrentPage(int scrollOffset)
@@ -86,6 +167,7 @@ namespace Toggl.Droid.Fragments.Calendar
         public override void OnDestroyView()
         {
             ViewModel?.DetachView();
+            dismissActionDisposeBag?.Dispose();
             DisposeBag.Dispose();
             DisposeBag = new CompositeDisposable();
             base.OnDestroyView();
