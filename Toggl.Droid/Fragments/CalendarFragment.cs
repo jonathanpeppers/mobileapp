9dcf0beb3aced8676e38a2ce7464032773ba0383
Daivid S. Silverio <daivid.silverio@gmail.com>
2019-10-16 21:52:27 -0400

    :sparkles: Add the calendar week stripe pager adapter

--- Toggl.Droid/Fragments/CalendarFragment.cs
+++ Toggl.Droid/Fragments/CalendarFragment.cs
@@ -1,10 +1,13 @@
 using Android.OS;
 using Android.Views;
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using Android.Runtime;
+using Android.Support.Constraints;
 using Android.Support.V4.App;
 using Android.Support.V4.View;
 using Toggl.Core;
@@ -13,6 +16,7 @@ using Toggl.Droid.Extensions;
 using Toggl.Droid.Fragments.Calendar;
 using Toggl.Droid.Helper;
 using Toggl.Droid.Presentation;
+using Toggl.Droid.ViewHolders;
 using Toggl.Droid.Views.Calendar;
 using Toggl.Shared.Extensions;
 using Toggl.Shared.Extensions.Reactive;
@@ -158,6 +162,90 @@ namespace Toggl.Droid.Fragments
 
             public void OnPageScrolled(int position, float positionOffset, int positionOffsetPixels)
             {
+            }
+        }
+        
+        private class CalendarWeekStripeAdapter : PagerAdapter
+        {
+            private readonly InputAction<CalendarWeeklyViewDayViewModel> dayInputAction;
+            private readonly Dictionary<int, CalendarWeekSectionViewHolder> pages = new Dictionary<int, CalendarWeekSectionViewHolder>();
+            private ImmutableList<ImmutableList<CalendarWeeklyViewDayViewModel>> weekSections = ImmutableList<ImmutableList<CalendarWeeklyViewDayViewModel>>.Empty;
+            private DateTime currentlySelectedDate = DateTime.Today;
+            
+            public CalendarWeekStripeAdapter(InputAction<CalendarWeeklyViewDayViewModel> dayInputAction)
+            {
+                this.dayInputAction = dayInputAction;
+            }
+
+            public override Java.Lang.Object InstantiateItem(ViewGroup container, int position)
+            {
+                var weekStripe = (ConstraintLayout)LayoutInflater.From(container.Context).Inflate(Resource.Layout.CalendarWeekStripeDaysView, container, false);
+                var weekSectionViewHolder = new CalendarWeekSectionViewHolder(weekStripe, dayInputAction);
+                var weekSection = weekSections[position];
+                
+                weekSectionViewHolder.InitDaysAndSelectedDate(weekSection, currentlySelectedDate);
+
+                pages[position] = weekSectionViewHolder;
+                
+                container.AddView(weekStripe);
+                return weekStripe;
+            }
+
+            public override void DestroyItem(ViewGroup container, int position, Java.Lang.Object @object)
+            {
+                pages.TryGetValue(position, out var page);
+                page?.Destroy();
+                container.RemoveView(@object as View);
+            }
+
+            public override bool IsViewFromObject(View view, Java.Lang.Object @object) 
+                => view == @object;
+
+            public override int Count => weekSections.Count;
+
+            public void UpdateWeekDays(ImmutableList<CalendarWeeklyViewDayViewModel> newWeekDays)
+            {
+                weekSections = createWeekSections(newWeekDays);
+                NotifyDataSetChanged();
+                for (var pageIndex = 0; pageIndex < weekSections.Count; pageIndex++)
+                {
+                    pages.TryGetValue(pageIndex, out var page);
+                    page?.UpdateDays(weekSections[pageIndex]);
+                }
+            }
+            
+            public void UpdateSelectedDay(DateTime newSelectedDate)
+            {
+                currentlySelectedDate = newSelectedDate;
+                for (var pageIndex = 0; pageIndex < weekSections.Count; pageIndex++)
+                {
+                    pages.TryGetValue(pageIndex, out var page);
+                    page?.UpdateCurrentlySelectedDate(newSelectedDate);
+                }
+            }
+
+            public int GetPageFor(DateTime selectedDate)
+            {
+                var currentWeekDays = weekSections;
+                for (var page = 0; page < currentWeekDays.Count; page++)
+                {
+                    var weekSection = currentWeekDays[page];
+                    if (selectedDate >= weekSection.First().Date && selectedDate <= weekSection.Last().Date)
+                        return page;
+                }
+
+                return 0;
+            }
+
+            private ImmutableList<ImmutableList<CalendarWeeklyViewDayViewModel>> createWeekSections(ImmutableList<CalendarWeeklyViewDayViewModel> newWeekDays)
+            {
+                var weeklySections = new List<ImmutableList<CalendarWeeklyViewDayViewModel>>();
+                for (var i = 0; i < newWeekDays.Count; i += 7)
+                {
+                    weeklySections.Add(newWeekDays.GetRange(i, 7));
+                }
+
+                return weeklySections.ToImmutableList();
             }
         }
     }
